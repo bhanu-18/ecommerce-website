78280ec9d443b40d0613be07131c581acd2e6879
bhanu-18 <bhanuteja0018@gmail.com>
2024-08-13 11:14:50 -0500

    Set up back-end with Express, MongoDB, and basic routes

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const authRoutes = require('./routes/auth');
+const productRoutes = require('./routes/product');
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
